81d8c53793bc0e08e01c8a862714bdbe868148c4
John Loverich <john.loverich@gmail.com>
2020-10-18 09:04:18 -0700

    Fixing layers for multiple input, updating git ignore.

--- functional_layers/LagrangePolynomial.py
+++ functional_layers/LagrangePolynomial.py
@@ -29,22 +29,24 @@ class LagrangePoly:
         return ans
 
     def interpolate(self, x, w):
-        # TODO: this can probably be made more efficient
-        # doing it this way since the multiplication keeps
-        # broadcasting where I don't want it to.
-        print('x.shape', x.shape)
-        batch_list = []
-        for batch in range(x.shape[0]):
-            out_list = []
-            for out in range(w.shape[2]):
-                b = 0
-                for inp in range(x.shape[1]):
-                    b += sum([self.basis(x[batch, inp], j)*w[batch, inp, out, j]
-                             for j in range(self.n)])
-                out_list.append(b)
-
-            batch_list.append(torch.tensor(out_list,requires_grad=True))
-
-        ans = torch.stack(batch_list)
-        print('ans.shape', ans.shape)
+        """
+        Args:
+            - x: size[batch, input]
+            - w: size[batch, input, output, basis]
+        Returns:
+            - result: size[batch, output]
+        """
+        mat = []
+        for j in range(self.n) :
+            basis_j = self.basis(x,j)
+            w_j = w[:,:,:,j]
+            
+            out_list=[]
+            for out in range(w.shape[2]) :
+                final = basis_j*w_j[:,:,out]
+                out_list.append(final)
+                
+            mat.append(torch.stack(out_list))
+        
+        ans = torch.transpose(torch.sum(torch.sum(torch.stack(mat),dim=0),dim=2),0,1)
         return ans
--- functional_layers/PolynomialLayers.py
+++ functional_layers/PolynomialLayers.py
@@ -7,68 +7,70 @@ from .LagrangePolynomial import *
 class Polynomial(nn.Module):
     def __init__(self, n, in_features, out_features):
         super().__init__()
-        self.poly=LagrangePoly(n)
-        self.n=n
+        self.poly = LagrangePoly(n)
+        self.n = n
 
-        self.w=torch.nn.Parameter(data = torch.Tensor(
-            out_features, in_features, n), requires_grad = True)
+        self.w = torch.nn.Parameter(data=torch.Tensor(
+            out_features, in_features, n), requires_grad=True)
         self.w.data.uniform_(-1, 1)
 
     def forward(self, x):
         # unfortunately we don't have automatic broadcasting yet
         # w = self.w.expand_as(x)
-        fx=self.poly.interpolate(x, self.w)
+        fx = self.poly.interpolate(x, self.w)
 
         return fx
+
 
 class PiecewisePolynomial(nn.Module):
     def __init__(self, n, in_features, out_features, segments):
         super().__init__()
-        self._poly=LagrangePoly(n)
-        self._n=n
-        self._segments=segments
-        self.in_features=in_features
-        self.w=torch.nn.Parameter(data = torch.Tensor(
-            out_features, in_features, ((n-1)*segments+1)), requires_grad = True)
+        self._poly = LagrangePoly(n)
+        self._n = n
+        self._segments = segments
+        self.in_features = in_features
+        self.w = torch.nn.Parameter(data=torch.Tensor(
+            out_features, in_features, ((n-1)*segments+1)), requires_grad=True)
         self.w.data.uniform_(-1, 1)
 
     def forward(self, x):
         # get the segment index
-        id_min=(((x+1.0)/2.0)*self._segments).long()
-        id_max=id_min+1
+        id_min = (((x+1.0)/2.0)*self._segments).long()
+        id_max = id_min+1
 
         # determine which weights are active
-        wid_min=(id_min*(self._n-1)).long()
-        wid_max=(id_max*(self._n-1)).long()+1
+        wid_min = (id_min*(self._n-1)).long()
+        wid_max = (id_max*(self._n-1)).long()+1
 
         # get the range of x in this segment
 
-        x_min=self._eta(id_min)
-        x_max=self._eta(id_max)
+        x_min = self._eta(id_min)
+        x_max = self._eta(id_max)
 
         # rescale to -1 to +1
-        x_in=2.0*((x-x_min)/(x_max-x_min))-1.0
+        x_in = 2.0*((x-x_min)/(x_max-x_min))-1.0
 
-        w_list=[]
+        w_list = []
         """
         for i in range(list(x_in.size())[0]):
             id_1=wid_min[i].numpy()[0]
             id_2=wid_max[i].numpy()[0]
+            
             w=self.w[:, id_1:id_2]
             w_list.append(w)
         """
 
-        for i in range(x_in.shape[0]): # batch size
+        for i in range(x_in.shape[0]):  # batch size
             out_list = []
-            for j in range (x_in.shape[1]) : #input size
-                id_1=wid_min[i].numpy()[j]
-                id_2=wid_max[i].numpy()[j]
-                w=self.w[:, j, id_1:id_2]
+            for j in range(x_in.shape[1]):  # input size
+                id_1 = wid_min[i].numpy()[j]
+                id_2 = wid_max[i].numpy()[j]
+                w = self.w[:, j, id_1:id_2]
                 out_list.append(w)
             w_list.append(torch.stack(out_list))
 
-        w_in=torch.stack(w_list)
-        fx=self._poly.interpolate(x_in, w_in)
+        w_in = torch.stack(w_list)
+        fx = self._poly.interpolate(x_in, w_in)
         return fx
 
     def _eta(self, index):
@@ -76,51 +78,52 @@ class PiecewisePolynomial(nn.Module):
         Arg:
             - index is the segment index
         """
-        eta=index/float(self._segments)
+        eta = index/float(self._segments)
         return eta*2-1
 
 
 class PiecewiseDiscontinuousPolynomial(nn.Module):
     def __init__(self, n, in_features, out_features, segments):
         super().__init__()
-        self._poly=LagrangePoly(n)
-        self._n=n
-        self._segments=segments
-        self.in_features=in_features
-        self.w=torch.nn.Parameter(data = torch.Tensor(
-            out_features, in_features, n*segments), requires_grad = True)
+        self._poly = LagrangePoly(n)
+        self._n = n
+        self._segments = segments
+        self.in_features = in_features
+        self.w = torch.nn.Parameter(data=torch.Tensor(
+            out_features, in_features, n*segments), requires_grad=True)
         self.w.data.uniform_(-1, 1)
 
     def forward(self, x):
         # determine which segment it is in
-        id_min=(((x+1.0)/2.0)*self._segments).long()
-        id_max=id_min+1
+        id_min = (((x+1.0)/2.0)*self._segments).long()
+        id_max = id_min+1
 
         # determine which weights are active
-        wid_min=(id_min*self._n).long()
-        wid_max=(id_max*self._n).long()
+        wid_min = (id_min*self._n).long()
+        wid_max = (id_max*self._n).long()
 
         # get the range of x in this segment
-        x_min=self._eta(id_min)
-        x_max=self._eta(id_max)
+        x_min = self._eta(id_min)
+        x_max = self._eta(id_max)
 
         # rescale to -1 to +1
-        x_in=2.0*((x-x_min)/(x_max-x_min))-1.0
-        w_list=[]
+        x_in = 2.0*((x-x_min)/(x_max-x_min))-1.0
+        w_list = []
 
-        for i in range(x_in.shape[0]): # batch size
+        # looks good
+        for i in range(x_in.shape[0]):  # batch size
             out_list = []
-            for j in range (x_in.shape[1]) : #input size
-                id_1=wid_min[i].numpy()[j]
-                id_2=wid_max[i].numpy()[j]
-                w=self.w[:, j, id_1:id_2]
+            for j in range(x_in.shape[1]):  # input size
+                id_1 = wid_min[i].numpy()[j]
+                id_2 = wid_max[i].numpy()[j]
+                w = self.w[:, j, id_1:id_2]
                 out_list.append(w)
             w_list.append(torch.stack(out_list))
 
-        w_in=torch.stack(w_list)
-        fx=self._poly.interpolate(x_in, w_in)
+        w_in = torch.stack(w_list)
+        fx = self._poly.interpolate(x_in, w_in)
         return fx
 
     def _eta(self, index):
-        eta=index/float(self._segments)
+        eta = index/float(self._segments)
         return eta*2-1
