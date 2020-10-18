74826a72047e4723c92531c2e54f5ce6329f4f14
John Loverich <john.loverich@gmail.com>
2020-10-18 11:07:27 -0700

    Invariant mnist now running on cpu.  Check gpu next.

--- invariant_mnist.py
+++ invariant_mnist.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import util
-from high_order_layers_torch.PolynomialLayers import PiecewiseDiscontinuousPolynomial
+from functional_layers.PolynomialLayers import PiecewisePolynomial
 
 transform = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
@@ -27,14 +27,15 @@ classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 
 
 class Net(LightningModule):
-    def __init__(self, poly_order, segments=2):
-        super(Net, self).__init__()
+    def __init__(self, poly_order, segments=2, batch_size=32):
+        super().__init__()
+        self._batch_size = batch_size
         self.criterion = nn.CrossEntropyLoss()
 
-        self.layer1 = PiecewiseDiscontinuousPolynomial(
+        self.layer1 = PiecewisePolynomial(
             poly_order+1, 784, 100, segments)
         self.layer2 = nn.LayerNorm(100)
-        self.layer3 = PiecewiseDiscontinuousPolynomial(
+        self.layer3 = PiecewisePolynomial(
             poly_order+1, 100, 10, segments)
         self.layer4 = nn.LayerNorm(10)
 
@@ -43,20 +44,24 @@ class Net(LightningModule):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-        return x
+        output = F.log_softmax(x,dim=1)
+        return output
 
-    def traning_step(self, batch, batch_idx):
+    def training_step(self, batch, batch_idx):
         x, y = batch
-        y_hat = self(x)
-        return {'loss': F.mse_loss(y_hat, y)}
+        x_new = x.view(x.shape[0], -1)
+        y_hat = self(x_new)
+        return F.cross_entropy(y_hat, y)
 
     def train_dataloader(self):
         trainset = torchvision.datasets.MNIST(
             root='./data', train=True, download=True, transform=transform)
-        return torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+        return torch.utils.data.DataLoader(trainset, batch_size=self._batch_size, shuffle=True, num_workers=2)
 
-    def configure_optim(self):
+    def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr=0.001)
 
 
-net = Net(poly_order=2, segments=2)
+trainer = Trainer(max_epochs=2,gpus=1)
+model = Net(poly_order=2, segments=2)
+trainer.fit(model)
