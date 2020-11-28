34d1a8f89987caac471dc4be3a53cbbfbdd8e91f
John Loverich <john.loverich@gmail.com>
2020-11-28 09:15:00 -0800

    Adding documentation to convolutional layers

--- high_order_layers_torch/FunctionalConvolution.py
+++ high_order_layers_torch/FunctionalConvolution.py
@@ -9,6 +9,12 @@ import torch
 
 class Expansion2d(nn.Module):
     def __init__(self, basis=None):
+        """
+        Expand an input by a function defined by basis.
+
+        Args :
+            - basis: function to expand input by.
+        """
         super().__init__()
         if basis == None:
             raise Exception(
@@ -40,6 +46,18 @@ class Expansion2d(nn.Module):
 class FourierConvolution2d(nn.Module):
 
     def __init__(self, n: int, in_channels: int, kernel_size: int, length: float = 2.0, rescale_output=False, *args, **kwargs):
+        """
+        Fourier series convolutional layer.
+
+        Args :
+            - n : number of fourier series components. n=1 is a constant, n=3 contains both first sin an consine components.
+            - in_channels : number of input channels
+            - kernel_size : size of the kernel
+            - length : Range of the polynomial interpolation points.  length = 2 implies [-1, 1] so the interpolation points
+                are in that range.  Anything outside that range could grow.
+            - rescale_output: If rescale output is True then the output is divided by the number of inputs for each output,
+                in effect taking the average.  This is generally not necessary for the fourier series.
+        """
         super().__init__()
         self.poly = Expansion2d(FourierExpand(n, length))
         self._channels = n*in_channels
@@ -57,11 +75,18 @@ class FourierConvolution2d(nn.Module):
 
 
 class PolynomialConvolution2d(nn.Module):
-    def __init__(self, n: int, in_channels: int, kernel_size: int, segments: int = 1, length: float = 2.0, rescale_output=False, *args, **kwargs):
+    def __init__(self, n: int, in_channels: int, kernel_size: int, length: float = 2.0, rescale_output=False, *args, **kwargs):
         """
-        TODO: remove "segments" and del from kwargs manually when used.
-        Segments is not used in this function, but is included in the parameter list 
-        so it isn't passed to Conv2d which does not take arbitrary keywords.
+        Polynomial convolutional layer.
+
+        Args :
+            - n : number of weights or nodes.  Polynomial order is n-1 so quadratic would be n=3.
+            - in_channels : number of input channels
+            - kernel_size : size of the kernel
+            - length : Range of the polynomial interpolation points.  length = 2 implies [-1, 1] so the interpolation points
+                are in that range.  Anything outside that range could grow.
+            - rescale_output: If rescale output is True then the output is divided by the number of inputs for each output,
+                in effect taking the average.
         """
         super().__init__()
         self.poly = Expansion2d(LagrangeExpand(n, length=length))
@@ -81,6 +106,19 @@ class PolynomialConvolution2d(nn.Module):
 
 class PiecewisePolynomialConvolution2d(nn.Module):
     def __init__(self, n: int, segments: int,  in_channels: int, kernel_size: int, length: float = 2.0, rescale_output: bool = False, *args, **kwargs):
+        """
+        Piecewise continuous polynomial convolutional layer.  The boundary between each polynomial are continuous. 
+
+        Args :
+            - n : number of weights or nodes.  Polynomial order is n-1 so quadratic would be n=3.
+            - segments: The number of segments in the piecewise polynomial.
+            - in_channels : number of input channels
+            - kernel_size : size of the kernel
+            - length : Range of the piecewise polynomial interpolation points.  length = 2 implies [-1, 1] so the interpolation points
+                are in that range.
+            - rescale_output: If rescale output is True then the output is divided by the number of inputs for each output,
+                in effect taking the average.
+        """
         super().__init__()
         self.poly = Expansion2d(
             PiecewisePolynomialExpand(n=n, segments=segments, length=length))
@@ -100,6 +138,18 @@ class PiecewisePolynomialConvolution2d(nn.Module):
 
 class PiecewiseDiscontinuousPolynomialConvolution2d(nn.Module):
     def __init__(self, n: int, segments: int,  in_channels: int, kernel_size: int, length: float = 2.0, rescale_output: bool = True, *args, **kwargs):
+        """
+        Discontinuous piecewise polynomial convolutional layer.  The boundary between each polynomial can be discontinuous. 
+        Args :
+            - n : number of weights or nodes.  Polynomial order is n-1 so quadratic would be n=3.
+            - segments: The number of segments in the piecewise polynomial.
+            - in_channels : number of input channels
+            - kernel_size : size of the kernel
+            - length : Range of the piecewise polynomial interpolation points.  length = 2 implies [-1, 1] so the interpolation points
+                are in that range.
+            - rescale_output: If rescale output is True then the output is divided by the number of inputs for each output,
+                in effect taking the average.
+        """
         super().__init__()
         self.poly = Expansion2d(
             PiecewiseDiscontinuousPolynomialExpand(n=n, segments=segments, length=length))
