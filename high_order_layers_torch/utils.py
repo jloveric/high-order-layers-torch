934d28ff18fba8534c7e9fc8198b20c433f729e3
John <john.loverich@gmail.com>
2022-06-21 20:38:58 -0700

    Adding position_from_mesh and test

--- high_order_layers_torch/utils.py
+++ high_order_layers_torch/utils.py
@@ -1,4 +1,7 @@
 import torch
+import math
+from torch import Tensor
+from typing import List
 
 
 def make_periodic(x, periodicity: float):
@@ -7,3 +10,69 @@ def make_periodic(x, periodicity: float):
     xp = torch.where(xp > periodicity, 2 * periodicity - xp, xp)
     xp = xp - 0.5 * periodicity
     return xp
+
+
+def positions_from_mesh(
+    width: int, height: int, rotations: int = 1, device="cuda", normalize: bool = False
+) -> List[Tensor]:
+    """
+    Produce a list of 2d "positions" from a mesh grid.  Assign a 1D value to each point
+    in the grid for example x, or y or the diagonal x+y... The outputs here can then be
+    used for positional encoding.
+
+    Args :
+        width : The width of the array in elements
+        height : The height of the array in elements
+        rotations : The number of rotations to return.
+            1 returns standard x, y (2 coordinate axis)
+            2 returns x, y, x+y, x-y (4 coordinate axis)
+            3 returns 6 coordinate axes (3 axes and the axis orthogonal to each)
+        normalize : If true the range will be normalized to [-1, 1] otherwise
+        it goes from [-max_dim//2, max_dim//2-1]
+    Returns :
+        A list of meshes and the rotated axis
+    """
+    max_dim = max(width, height)
+    ratio = max_dim / (max_dim + 1)
+    scale = max_dim / (max_dim - 1)
+
+    # Create and center the coordinates
+    # TODO: These don't have the ranges I thought so they will need to be fixed.
+    xv, yv = torch.meshgrid([torch.arange(width), torch.arange(height)])
+    xv = xv.to(device=device)
+    yv = yv.to(device=device)
+    xv = scale * xv
+    yv = scale * yv
+
+    # Coordinate values range from
+    line_list = []
+    for i in range(rotations):
+        theta = (math.pi / 2.0) * (i / rotations)
+        rot_x = math.cos(theta)
+        rot_y = math.sin(theta)
+
+        # Add the line and the line orthogonal
+        r1 = rot_x * xv + rot_y * yv
+        r1_max = torch.max(r1)
+        r1_min = torch.min(r1)
+        dr1 = r1_max - r1_min
+
+        r2 = rot_x * xv - rot_y * yv
+        r2_max = torch.max(r2)
+        r2_min = torch.min(r2)
+        dr2 = r2_max - r2_min
+
+        # Rescale these so they have length segments
+        # and are centered at (0,0)
+        r1 = ((r1 - r1_min) / dr1 - 0.5) * max_dim
+        r2 = ((r2 - r2_min) / dr2 - 0.5) * max_dim
+
+        # We want the values to range from -1 to 1
+        if normalize is True:
+            r1 = 2 * (r1 + max_dim / 2) / max_dim - 1
+            r2 = 2 * (r2 + max_dim / 2) / max_dim - 1
+
+        line_list.append(r1)
+        line_list.append(r2)
+
+    return line_list
---
+++ tests/test_utils.py
@@ -0,0 +1,26 @@
+import os
+import pytest
+from high_order_layers_torch.utils import make_periodic, positions_from_mesh
+import torch
+
+
+def test_positions_from_mesh():
+    width = 10
+    height = 10
+    rotations = 2
+    positions = positions_from_mesh(
+        width=width, height=height, rotations=rotations, normalize=True, device="cpu"
+    )
+
+    assert len(positions) == 4
+    for i in range(len(positions)):
+        assert torch.all(positions[i] >= -1.0).item() is True
+        assert torch.all(positions[i] <= 1.0).item() is True
+
+    positions = positions_from_mesh(
+        width=width, height=height, rotations=rotations, normalize=False, device="cpu"
+    )
+    assert len(positions) == 4
+    for i in range(len(positions)):
+        assert torch.all(positions[i] >= -5.0).item() is True
+        assert torch.all(positions[i] <= 5.0).item() is True
