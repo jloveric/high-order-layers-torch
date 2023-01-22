Adding networks with fixed rotation

high_order_layers_torch/layers.py
@@ -1,3 +1,5 @@
+import math
+
 import torch.nn as nn
 from torch.nn import Linear
 
@@ -5,7 +7,7 @@ from .FunctionalConvolution import *
 from .FunctionalConvolutionTranspose import *
 from .PolynomialLayers import *
 from .ProductLayer import *
-from .utils import max_abs_normalization, l2_normalization, max_abs_normalization_nd
+from .utils import l2_normalization, max_abs_normalization, max_abs_normalization_nd
 
 
 class MaxAbsNormalization(nn.Module):
@@ -98,6 +100,57 @@ convolutional_layers = {
 convolutional_transpose_layers = {
     "continuous2d": PiecewisePolynomialConvolutionTranspose2d
 }
+
+
+def fixed_rotation_layer(n: int, rotations: int = 2):
+    """
+    Take n inputs and compute all the variations, n_i+n_j, n_i-n_j
+    and create a layer that computes these with fixed weights. For
+    n=2, and rotations=2 outputs [x, t, a(x+t), a(x-t)].  Returns a fixed
+    linear rotation layer (one that is not updated by gradients)
+    Args :
+        - n: The number of inputs, would be 2 for (x, t)
+        - rotations: Number of rotations to apply pair by based on the inputs. So
+        for input [x, y] and rotations=3, rotations are [x, y,a*(x+t), a*(x-t) ]
+    Returns :
+        A tuple containing the rotation layer and the output width of the layer
+    """
+
+    if rotations < 1:
+        raise ValueError(
+            f"Rotations must be 1 or greater. 1 represents no additional rotations. Got rotations={rotations}"
+        )
+
+    combos = []
+    for i in range(n):
+        for j in range(i + 1, n):
+            for r in range(rotations):
+
+                # We need to add rotations from each of 2 quadrants
+                for t in range(2):
+                    a = t * math.pi / 2.0
+
+                    temp = [0] * n
+
+                    theta = a + math.pi * (r / rotations)
+                    rot_x = math.cos(theta)
+                    rot_y = math.sin(theta)
+
+                    # Add the line and the line orthogonal
+                    temp[i] += rot_x
+                    temp[j] += rot_y
+
+                    combos.append(temp)
+
+    # 2 inputs, 1 rotation -> 2 combos
+    # 2 inputs, 2 rotations -> 4 combos
+    # 2 inputs, 3 rotations -> 6 combos
+    # 2 inputs, 4 rotations -> 8 combos
+    output_width = n * (n - 1) * rotations
+    layer = torch.nn.Linear(n, n * (n - 1) * rotations, bias=False)
+    weights = torch.tensor(combos)
+    layer.weight = torch.nn.Parameter(weights, requires_grad=False)
+    return layer, output_width
 
 
 def high_order_fc_layers(layer_type: str, **kwargs):

high_order_layers_torch/networks.py
@@ -10,16 +10,17 @@ from torch import Tensor
 from torch.nn import Linear
 
 from high_order_layers_torch.layers import (
+    fixed_rotation_layer,
     high_order_convolution_layers,
     high_order_convolution_transpose_layers,
     high_order_fc_layers,
 )
 from high_order_layers_torch.PolynomialLayers import (
+    PiecewiseDiscontinuous,
+    PiecewisePolynomial,
     interpolate_polynomial_layer,
     refine_polynomial_layer,
     smooth_discontinuous_layer,
-    PiecewiseDiscontinuous,
-    PiecewisePolynomial,
 )
 
 logger = logging.getLogger(__name__)
@@ -738,6 +739,78 @@ class VanillaVAE(LightningModule):
 class HighOrderMLPMixerBlock(nn.Module):
     # Follow this block https://papers.nips.cc/paper/2021/file/cba0a4ee5ccd02fda0fe3f9a3e7b89fe-Paper.pdf
     pass
+
+
+def transform_mlp(
+    layer_type: str,
+    in_width: int,
+    hidden_width: int,
+    out_width: int,
+    n: int,
+    n_in: int,
+    n_hidden: int,
+    n_out: int,
+    in_segments: int,
+    hidden_segments: int,
+    out_segments: int,
+    hidden_layers: int,
+    scale: float,
+    periodicity: float,
+    normalization: torch.nn.Module,
+    rotations: int,
+) -> torch.nn.Module:
+
+    fixed_input, fixed_output_width = fixed_rotation_layer(
+        n=in_width, rotations=rotations
+    )
+
+    mlp = HighOrderMLP(
+        layer_type=layer_type,
+        n=n,
+        n_in=n_in,
+        n_hidden=n_hidden,
+        n_out=n_out,
+        in_width=fixed_output_width,
+        in_segments=in_segments,
+        out_width=out_width,
+        out_segments=out_segments,
+        hidden_width=hidden_width,
+        hidden_layers=hidden_layers,
+        hidden_segments=hidden_segments,
+        normalization=normalization,
+        scale=scale,
+        periodicity=periodicity,
+    )
+    tl = [fixed_input, mlp]
+    model = torch.nn.Sequential(*tl)
+    return model
+
+
+def transform_low_mlp(
+    in_width: int,
+    hidden_width: int,
+    out_width: int,
+    hidden_layers: int,
+    non_linearity: None,
+    normalization: torch.nn.Module,
+    rotations: int,
+) -> torch.nn.Module:
+
+    fixed_input, fixed_output_width = fixed_rotation_layer(
+        n=in_width, rotations=rotations
+    )
+
+    mlp = LowOrderMLP(
+        in_width=fixed_output_width,
+        out_width=out_width,
+        hidden_width=hidden_width,
+        hidden_layers=hidden_layers,
+        non_linearity=non_linearity,
+        normalization=normalization,
+    )
+    tl = [fixed_input, mlp]
+    model = torch.nn.Sequential(*tl)
+    return model
 
 
 def interpolate_high_order_mlp(