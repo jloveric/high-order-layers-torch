53e3c3209d5b114c281b90d06b6888f04c120560
John <john.loverich@gmail.com>
2023-12-22 06:58:24 -0800

    Add test to verify gradients computed where I think they are

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -5,7 +5,7 @@ import pytest
 from high_order_layers_torch.FunctionalConvolution import *
 from high_order_layers_torch.LagrangePolynomial import *
 from high_order_layers_torch.LagrangePolynomial import LagrangePoly
-
+import torch.nn.functional as F
 from high_order_layers_torch.layers import (
     L2Normalization,
     MaxAbsNormalization,
@@ -46,6 +46,32 @@ def test_constant_polynomial():
     x = torch.tensor([[0.5]])
     ans = poly.interpolate(x, w)
     assert abs(ans - w) < 1.0e-6
+
+
+def test_activation_grad():
+    """
+    Make sure only a subset of the values are being used in a standard
+    weight update
+    """
+
+    layer = PiecewiseDiscontinuousPolynomial(
+        n=2, in_features=1, out_features=1, segments=10
+    )
+    model = nn.Sequential(layer)
+
+    # Activate only on value
+    x = torch.ones(1, 1) * 0.5
+    target = torch.ones(1, 1) * 0.2
+    out = model(x)
+    loss = F.mse_loss(out, target)
+    loss.backward()
+
+    evaluated_grads = layer.w.grad[layer.w.grad != 0.0]
+    print("evaluated_grads", evaluated_grads)
+
+    assert evaluated_grads.shape == torch.Size([2])
+    assert layer.w.grad.shape == torch.Size([1, 1, 10 * 2])
+    print("weight gradients", layer.w.grad)
 
 
 def test_compare():
